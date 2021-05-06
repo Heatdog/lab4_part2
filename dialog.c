9fbee8eea334f75f9ac956eb19ba5fe3f4ccba9b
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-05-06 23:52:00 +0300

    Create dialog.c

---
+++ dialog.c
@@ -0,0 +1,150 @@
+//
+// Created by yura on 04.05.2021.
+//
+
+#include "dialog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+const char *msgs[] = {"0. Quit", "1. Add", "2. Find", "3. Delete", "4. Show", "5. Find the most distance element from (0;0)","6. Timing"};
+const int N =sizeof(msgs) / sizeof(msgs[0]);
+
+void dialog(Tree* tree)
+{
+    int rc = 0;
+    while (1) {
+        printf("\n-------------------\n");
+        for (int i = 0; i < N; i++) {
+            puts(msgs[i]);
+        }
+        puts("Make your choice: --> ");
+        scan_int(&rc);
+        switch (rc) {
+            case 0:{
+                return;
+            }
+            case 1:{
+                D_Add(tree);
+                break;
+            }
+            case 2:{
+                D_Find(tree);
+                break;
+            }
+            case 3:{
+                //D_Delete(tree);
+                break;
+            }
+            case 4:{
+                D_Show(tree);
+                break;
+            }
+            case 5:{
+                //D_Distance(tree);
+                break;
+            }
+            default:{
+                //D_Timing();
+                break;
+            }
+        }
+    }
+}
+
+
+void scan_int(int* n) {
+    int i = 0;
+    do {
+        i = scanf("%d", n);
+        if (i != 1) {
+            printf("You`re wrong! Please, try again!\n");
+            scanf("%*c");
+        }
+    } while (i != 1);
+}
+
+char* scan_string(char* n) {
+    int k = 0;
+    n = (char*)malloc(100);
+    do {
+        k = scanf("%s", n);
+        if (k != 1) {
+            printf("Please, try again!\n");
+        }
+    } while (k != 1);
+    unsigned int i = strlen(n);
+    n = (char*)realloc(n, (i + 1) * sizeof(char));
+    return n;
+}
+
+Info* scan_info(Info* info) {
+    info = (Info*)malloc(sizeof(Info));
+    printf("Please, enter x\nEnter-->");
+    scan_int(&info->keys[0]);
+    printf("Please, enter y\nEnter-->");
+    scan_int(&info->keys[1]);
+    printf("Please, enter message (2 string)\n");
+    printf("First string-->");
+    info->text1 = scan_string(info->text1);
+    printf("Second string-->");
+    info->text2 = scan_string(info->text2);
+    info->next = NULL;
+    info->next_in_list = NULL;
+    info->release = 0;
+    return info;
+}
+
+void D_Add(Tree* tree){
+    Info *info = NULL;
+    info = scan_info(info);
+    node_add(tree, info);
+}
+
+
+void D_Show(Tree* tree){
+    printf("\n////////////////////// Tree //////////////////////\n\n");
+    print_tree(tree->node, 0);
+}
+
+/*void D_Timing(){
+    Tree *tree = NULL;
+    tree = new_tree(tree);
+    Info *info = NULL;
+    int count, key[2];
+    printf("Enter count of repeating\nEnter-->");
+    scan_int(&count);
+    printf("Start testing...\n");
+    clock_t begin_add = clock();
+    for (int i = 0; i < count; i++){
+        key[0] = rand();
+        key[1] = rand();
+        info = info_rand(info, key);
+        tree->list = insert_list(tree->list, info);
+    }
+    tree->node->list = copy_list(tree->list);
+    insert_kd_tree(tree->node, 0);
+    clock_t end_add = clock();
+    double time_spent_add = (double )(end_add - begin_add)/CLOCKS_PER_SEC;
+    printf("Testing complete, result of adding-->%f", time_spent_add);
+}*/
+
+
+void D_Find(Tree* tree){
+    Info *info = NULL;
+    info = (Info*)malloc(sizeof (Info));
+    printf("Please, enter first key\nEnter-->");
+    scan_int(&info->keys[0]);
+    printf("Please, second first key\nEnter-->");
+    scan_int(&info->keys[1]);
+    info = find_info(tree->node, info, 0);
+    if (info == NULL){
+        printf("We can`t find this element!");
+    } else{
+        show_info(info);
+    }
+}
+
+
+/////////////////////////////////////
