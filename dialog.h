f487f4de2986e83e8aaad42fb3e7cbd7aa77058b
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-05-06 23:52:05 +0300

    Create dialog.h

---
+++ dialog.h
@@ -0,0 +1,20 @@
+//
+// Created by yura on 04.05.2021.
+//
+
+#ifndef LAB4B_1_DIALOG_H
+#define LAB4B_1_DIALOG_H
+
+#include "tree.h"
+
+void dialog(Tree*);
+void scan_int(int*);
+void D_Add(Tree*);
+void D_Show(Tree*);
+void D_Timing();
+void D_Find(Tree*);
+char *scan_string(char *);
+Info *scan_info(Info*);
+Info *copy_info(Info*);
+
+#endif //LAB4B_1_DIALOG_H
