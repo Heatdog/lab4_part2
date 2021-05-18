ece9a75587e400e58b2d1ec653c3cf4e2ec0b0a6
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-05-18 22:24:06 +0300

    Update tree.c

--- tree.c
+++ tree.c
@@ -8,21 +8,22 @@
 #include <math.h>
 
 
-Tree *new_tree(Tree* tree){
-    tree = (Tree*)malloc(sizeof (Tree));
+Tree* new_tree(Tree* tree) {
+    tree = (Tree*)malloc(sizeof(Tree));
     tree->node = NULL;
     return tree;
 }
 
 ////////////////////////////////////////////////////////
 
-List *insert_list(List* list, Info* info){
-    if (list == NULL){
-        list = (List*)malloc(sizeof (List));
+List* insert_list(List* list, Info* info) {
+    if (list == NULL) {
+        list = (List*)malloc(sizeof(List));
         list->head = info;
         list->tail = info;
         list->n = 1;
-    } else{
+    }
+    else {
         list->tail->next = info;
         list->tail = info;
         info->next = NULL;
@@ -35,30 +36,30 @@ List *insert_list(List* list, Info* info){
 
 
 
-Node *new_node(Node* node, Info* info, unsigned int axis){
-    node = (Node*)malloc(sizeof (Node));
+Node* new_node(Node* node, Info* info, unsigned int axis) {
+    node = (Node*)malloc(sizeof(Node));
     node->left = NULL;
     node->right = NULL;
     node->parent = NULL;
-    node->location = info->keys[(axis+1)%2];
+    node->location = info->keys[(axis + 1) % 2];
     node->list = NULL;
     node->list = insert_list(node->list, info);
     return node;
 }
 
 
-void printList(List* list, int offset){
-    if (list == NULL){
+void printList(List* list, int offset) {
+    if (list == NULL) {
         return;
     }
-    Info *ptr = list->head;
-    Info *info = list->head;
-    if (ptr != NULL){
-        for (int k = 0; k < offset; k++){
+    Info* ptr = list->head;
+    Info* info = list->head;
+    if (ptr != NULL) {
+        for (int k = 0; k < offset; k++) {
             printf("\t");
         }
         printf("[%d;%d] ", info->keys[0], info->keys[1]);
-        while (info != NULL){
+        while (info != NULL) {
             printf("(%s, %s) ", info->text1, info->text2);
             info = info->next;
         }
@@ -68,31 +69,33 @@ void printList(List* list, int offset){
 
 //////////////////////////////////////////////////////////////////////////
 
-void print_tree(Node *node, int offset){
-    if (node == NULL){
+void print_tree(Node* node, int offset) {
+    if (node == NULL) {
         return;
-    } else{
-        print_tree(node->left, offset+1);
+    }
+    else {
+        print_tree(node->left, offset + 1);
         show_node(node, offset);
         printf("\n");
-        print_tree(node->right, offset+1);
+        print_tree(node->right, offset + 1);
     }
 }
 
-void show_node(Node* node, int offset){
-    if (node->right == NULL && node->left == NULL){
+void show_node(Node* node, int offset) {
+    if (node->right == NULL && node->left == NULL) {
         printList(node->list, offset);
-    } else{
-        for (int k = 0; k < offset; k++){
+    }
+    else {
+        for (int k = 0; k < offset; k++) {
             printf("\t");
         }
         printf("%d", node->location);
     }
 }
 
-void show_info(Info* info){
+void show_info(Info* info) {
     printf("[%d;%d]", info->keys[0], info->keys[1]);
-    while (info != NULL){
+    while (info != NULL) {
         printf(" (%s,%s) ", info->text1, info->text2);
         info = info->next;
     }
@@ -103,45 +106,52 @@ void show_info(Info* info){
 ///////////////////////////////////////////////////////////////////
 
 
-void node_add(Tree* tree, Info* info){
-    if (tree->node == NULL){
+void node_add(Tree* tree, Info* info) {
+    if (tree->node == NULL) {
         tree->node = new_node(tree->node, info, 1);
-    } else{
-        add_no_null(tree->node, info , 0);
+    }
+    else {
+        add_no_null(tree->node, info, 0);
     }
 }
 
-void add_no_null(Node* node, Info* info, unsigned int depth){
-    unsigned int axis = depth%2;
-    if (info->keys[axis] >= node->location){
-        if (node->list->head->keys[0] == info->keys[0] && node->list->head->keys[1] == info->keys[1] && node->left != NULL && node->right != NULL){
-            add_no_null(node->left, info, depth+1);
-        } else if (node->list->head->keys[0] == info->keys[0] && node->list->head->keys[1] == info->keys[1] && node->left == NULL && node->right == NULL){
-           node->list = insert_list(node->list, info);
-           return;
-       } else if (node->right == NULL && node->list->head->keys[axis] != info->keys[axis]){
-           node->right = new_node(node->right, info, depth);
-           node->right->parent = node;
-       } else if (node->right == NULL && node->list->head->keys[axis] == info->keys[axis]){
+void add_no_null(Node* node, Info* info, unsigned int depth) {
+    unsigned int axis = depth % 2;
+    if (info->keys[axis] >= node->location) {
+        if (node->list->head->keys[0] == info->keys[0] && node->list->head->keys[1] == info->keys[1] && node->left != NULL && node->right != NULL) {
+            add_no_null(node->left, info, depth + 1);
+        }
+        else if (node->list->head->keys[0] == info->keys[0] && node->list->head->keys[1] == info->keys[1] && node->left == NULL && node->right == NULL) {
+            node->list = insert_list(node->list, info);
+            return;
+        }
+        else if (node->right == NULL && node->list->head->keys[axis] != info->keys[axis]) {
+            node->right = new_node(node->right, info, depth);
+            node->right->parent = node;
+        }
+        else if (node->right == NULL && node->list->head->keys[axis] == info->keys[axis]) {
             node->right = new_node(node->right, node->list->head, depth);
             node->right->parent = node;
-            add_no_null(node->right, info, depth+1);
+            add_no_null(node->right, info, depth + 1);
             return;
-       }else{
-            add_no_null(node->right, info, depth+1);
-       }
-       if (node->left == NULL){
-           node->left = new_node(node->left, node->list->head, depth);
-           node->left->parent = node;
-       }
-    } else{
-        if (node->left == NULL){
+        }
+        else {
+            add_no_null(node->right, info, depth + 1);
+        }
+        if (node->left == NULL) {
+            node->left = new_node(node->left, node->list->head, depth);
+            node->left->parent = node;
+        }
+    }
+    else {
+        if (node->left == NULL) {
             node->left = new_node(node->left, info, depth);
             node->left->parent = node;
-        } else{
-            add_no_null(node->left, info, depth+1);
         }
-        if (node->right == NULL){
+        else {
+            add_no_null(node->left, info, depth + 1);
+        }
+        if (node->right == NULL) {
             node->right = new_node(node->right, node->list->head, depth);
             node->right->parent = node;
         }
@@ -149,36 +159,40 @@ void add_no_null(Node* node, Info* info, unsigned int depth){
 }
 
 
-Info *find_info(Node *node, Info* info, unsigned int depth){
-    if (node == NULL){
+Info* find_info(Node* node, Info* info, unsigned int depth) {
+    if (node == NULL) {
         return NULL;
     }
-    unsigned int axis = depth%2;
-    if (info->keys[axis] > node->location && node->location != 0){
-        return find_info(node->right, info, depth+1);
-    } else if (info->keys[axis] <= node->location && node->location != 0 && info->keys[0] != node->list->head->keys[0] && info->keys[1] != node->list->head->keys[1]){
-        return find_info(node->left, info, depth+1);
+    unsigned int axis = depth % 2;
+    if (info->keys[axis] > node->location && node->location != 0) {
+        return find_info(node->right, info, depth + 1);
     }
-    if (info->keys[0] == node->list->head->keys[0] && info->keys[1] == node->list->head->keys[1]){
+    else if (info->keys[axis] <= node->location && node->location != 0 && info->keys[0] != node->list->head->keys[0] && info->keys[1] != node->list->head->keys[1]) {
+        return find_info(node->left, info, depth + 1);
+    }
+    if (info->keys[0] == node->list->head->keys[0] && info->keys[1] == node->list->head->keys[1]) {
         return node->list->head;
-    } else{
+    }
+    else {
         return NULL;
     }
 }
 
-Node *find_node(Node *node, Info* info, unsigned int depth){
-    if (node == NULL){
+Node* find_node(Node* node, Info* info, unsigned int depth) {
+    if (node == NULL) {
         return NULL;
     }
-    unsigned int axis = depth%2;
-    if (info->keys[axis] > node->location){
-        return find_node(node->right, info, depth+1);
-    } else if (info->keys[axis] <= node->location && node->right != NULL && node->left != NULL){
-        return find_node(node->left, info, depth+1);
+    unsigned int axis = depth % 2;
+    if (info->keys[axis] > node->location) {
+        return find_node(node->right, info, depth + 1);
     }
-    if (info->keys[0] == node->list->head->keys[0] && info->keys[1] == node->list->head->keys[1]){
+    else if (info->keys[axis] <= node->location && node->right != NULL && node->left != NULL) {
+        return find_node(node->left, info, depth + 1);
+    }
+    if (info->keys[0] == node->list->head->keys[0] && info->keys[1] == node->list->head->keys[1]) {
         return node;
-    } else{
+    }
+    else {
         return NULL;
     }
 }
@@ -186,42 +200,43 @@ Node *find_node(Node *node, Info* info, unsigned int depth){
 /////////////////////////////////////////////////////////////////
 
 
-void delete_tree(Tree* tree){
+void delete_tree(Tree* tree) {
     delete_node(tree->node);
     free(tree);
 }
 
-void delete_node(Node* node){
-    if (node == NULL){
+void delete_node(Node* node) {
+    if (node == NULL) {
         return;
-    } else{
+    }
+    else {
         delete_node(node->left);
         delete_node(node->right);
-        if (node->left == NULL && node->right == NULL){
+        if (node->left == NULL && node->right == NULL) {
             delete_node_next(node->list->head);
-            free(node->list);
         }
+        free(node->list);
         free(node);
     }
 }
 
-int delete_list(List* list, int release){
-    Info *ptr = NULL, *ptr_prev = NULL;
+int delete_list(List* list, int release) {
+    Info* ptr = NULL, * ptr_prev = NULL;
     ptr = list->head;
-    for (int i = 1; i < release; i++){
+    for (int i = 1; i < release; i++) {
         ptr_prev = ptr;
         ptr = ptr->next;
-        if (ptr == NULL){
+        if (ptr == NULL) {
             return 0;
         }
     }
-    if (list->head == list->tail){
+    if (list->head == list->tail) {
         free(ptr->text1);
         free(ptr->text2);
         free(ptr);
         return 1;
     }
-    if (ptr == list->head){
+    if (ptr == list->head) {
         list->head = ptr->next;
         free(ptr->text1);
         free(ptr->text2);
@@ -229,7 +244,7 @@ int delete_list(List* list, int release){
         list->n--;
         return 0;
     }
-    if (ptr == list->tail){
+    if (ptr == list->tail) {
         list->tail = ptr_prev;
         ptr_prev->next = NULL;
         free(ptr->text1);
@@ -247,10 +262,11 @@ int delete_list(List* list, int release){
 }
 
 
-void delete_node_next(Info*  info){
-    if (info == NULL){
+void delete_node_next(Info* info) {
+    if (info == NULL) {
         return;
-    } else{
+    }
+    else {
         delete_node_next(info->next);
         free(info->text1);
         free(info->text2);
@@ -258,43 +274,50 @@ void delete_node_next(Info*  info){
     }
 }
 
-void delete_element(Tree *tree, Info* info, int release){
-    Node *node = NULL, *parent = NULL;
-    Node *right = NULL, *left = NULL;
-    Node *node_parent = NULL, *reset = NULL;
-    Node *min = NULL;
+void delete_element(Tree* tree, Info* info, int release) {
+    Node* node = NULL, * parent = NULL;
+    Node* right = NULL, * left = NULL;
+    Node* node_parent = NULL, * reset = NULL;
+    Node* min = NULL;
     node = find_node(tree->node, info, 0);
-    if (node == NULL){
+    if (node == NULL) {
         return;
     }
     parent = node->parent;
-    if (parent == NULL){
+    if (parent == NULL) {
         int k = delete_list(node->list, release);
-        if (k == 1){
+        if (k == 1) {
+            free(node->list);
             free(node);
             tree->node = NULL;
         }
-    } else if (parent->right == node){
+    }
+    else if (parent->right == node) {
         int k = delete_list(node->list, release);
-        if (k == 1){
+        if (k == 1) {
             node_parent = parent->parent;
             left = parent->left;
-            if (node_parent == NULL){
+            if (node_parent == NULL) {
                 tree->node = left;
                 left->parent = NULL;
-            } else{
-                if (node_parent->left == parent){
+            }
+            else {
+                if (node_parent->left == parent) {
                     node_parent->left = left;
-                } else{
+                }
+                else {
                     node_parent->right = left;
                 }
                 parent->left->parent = node_parent;
             }
             reset_location(left);
+            free(parent->list);
             free(parent);
+            free(node->list);
             free(node);
         }
-    } else{
+    }
+    else {
         int k = delete_list(node->list, release);
         if (k == 1) {
             right = parent->right;
@@ -303,11 +326,13 @@ void delete_element(Tree *tree, Info* info, int release){
                 tree->node = min;
                 min->parent = NULL;
                 reset_location(min);
-            } else {
+            }
+            else {
                 reset = min->parent->right;
                 if (min->parent->parent->right == min->parent) {
                     min->parent->parent->right = reset;
-                } else {
+                }
+                else {
                     min->parent->parent->left = reset;
                 }
                 reset->parent = min->parent->parent;
@@ -317,35 +342,40 @@ void delete_element(Tree *tree, Info* info, int release){
                     if (parent->location == node->list->head->keys[0]) {
                         parent->location = min->list->head->keys[0];
                         min->location = min->list->head->keys[1];
-                    } else {
+                    }
+                    else {
                         parent->location = min->list->head->keys[1];
                         min->location = min->list->head->keys[0];
                     }
-                    free(min->parent);
                     min->parent = parent;
                 }
             }
+            free(parent->list);
+            free(parent);
+            free(node->list);
             free(node);
         }
     }
 }
 
-Node *find_min(Node* node){
-    while (node->left != NULL){
+Node* find_min(Node* node) {
+    while (node->left != NULL) {
         node = node->left;
     }
     return node;
 }
 
-void reset_location(Node* node){
-    if (node == NULL){
+void reset_location(Node* node) {
+    if (node == NULL) {
         return;
-    } else{
+    }
+    else {
         reset_location(node->left);
         reset_location(node->right);
-        if (node->location == node->list->head->keys[0]){
+        if (node->location == node->list->head->keys[0]) {
             node->location = node->list->head->keys[1];
-        } else{
+        }
+        else {
             node->location = node->list->head->keys[0];
         }
     }
@@ -354,11 +384,12 @@ void reset_location(Node* node){
 
 /////////////////////////////////////////////////////////////////
 
-void find_max(Node* node, int key[]){
-    if (node == NULL){
+void find_max(Node* node, int key[]) {
+    if (node == NULL) {
         return;
-    } else{
-        if (node->left == NULL && node->right == NULL && math(node->list->head->keys[0], node->list->head->keys[1]) > math(key[0], key[1])){
+    }
+    else {
+        if (node->left == NULL && node->right == NULL && math(node->list->head->keys[0], node->list->head->keys[1]) > math(key[0], key[1])) {
             key[0] = node->list->head->keys[0];
             key[1] = node->list->head->keys[1];
         }
@@ -367,6 +398,6 @@ void find_max(Node* node, int key[]){
     }
 }
 
-double math(int a, int b){
-    return sqrt(a*a + b*b);
+double math(int a, int b) {
+    return sqrt(a * a + b * b);
 }
